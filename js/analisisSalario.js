fc37c19086089a323208a3d9540765efa828a3cf
AJHernandezA <adolfohernandezaponte@hotmail.com>
2021-09-29 10:37:33 -0500

    Investment page

--- js/analisisSalario.js
+++ js/analisisSalario.js
@@ -1,64 +1,78 @@
-const salariosCol = colombia.map(
-    function (persona){
-        return persona.salary;
+function investmentWithSimpleInterest(
+    startAmount,
+    interestPercentage,
+    timeYears
+  ) {
+    let capital = Number(startAmount);
+    let tiempo = Number(timeYears);
+    let tasaDeInteres = Number(interestPercentage / 100);
+    // Calculamos el interes simple
+    let interesSimple = capital * tasaDeInteres * tiempo;
+    let montoFinal = capital + interesSimple;
+    return Number(montoFinal);
+  }
+  function investmentWithCompoundInterest(
+    startAmount,
+    interestPercentage,
+    timeYears
+  ) {
+    let capital = Number(startAmount);
+    let tiempo = Number(timeYears);
+    let tasaDeInteres = Number(interestPercentage / 100);
+    // Calculamos el monto final con interes compuesto
+    let montoFinal = capital * (1 + tasaDeInteres) ** tiempo;
+    return Number(montoFinal);
+  }
+
+function reset() {
+    const inputAmount = document.getElementById("input-amount");
+    const inputInterest = document.getElementById("input-interest");
+    const inputTime = document.getElementById("input-time");
+    const inputTypeInterest = document.getElementById("type-interest");
+    const textResultado = document.getElementById("answer");
+    let montoValue = inputAmount.value;
+    let interesValue = inputInterest.value;
+    let tiempoValue = inputTime.value;
+    let tipoInteresValue = inputTypeInterest.value;
+  
+    montoValue = "";
+    interesValue = "";
+    tiempoValue = "";
+    tipoInteresValue = "simple";
+  }
+  
+  // Logica :D
+  function mostrarMontoInversion() {
+    const inputAmount = document.getElementById("input-amount");
+    const inputInterest = document.getElementById("input-interest");
+    const inputTime = document.getElementById("input-time");
+    const inputTypeInterest = document.getElementById("type-interest");
+    const textResultado = document.getElementById("answer");
+    let montoValue = inputAmount.value;
+    let interesValue = inputInterest.value;
+    let tiempoValue = inputTime.value;
+    let tipoInteresValue = inputTypeInterest.value;
+    if (montoValue !== "" && interesValue !== "" && tiempoValue !== "") {
+      let resultado = 0;
+      if (tipoInteresValue === "simple") {
+        resultado = investmentWithSimpleInterest(
+          montoValue,
+          interesValue,
+          tiempoValue
+        );
+        resultado = resultado.toFixed(2);
+        textResultado.innerHTML = `Your investment with initial capital of $${montoValue} and simple annual interest of ${interesValue}% over ${tiempoValue} years will be <span>$${resultado}</span>`;
+      }
+      if (tipoInteresValue === "compound") {
+        resultado = investmentWithCompoundInterest(
+          montoValue,
+          interesValue,
+          tiempoValue
+        );
+        resultado = resultado.toFixed(2);
+        textResultado.innerHTML = `Your investment with initial capital of $${montoValue} and compound annual interest of ${interesValue}% over ${tiempoValue} years will be <span>$${resultado}</span>`;
+      }
+    } else {
+      textResultado.innerHTML = `Missing data `;
     }
-);
-
-const salariosColSorted = salariosCol.sort(
-    function (salaryA,salaryB) {
-        return salaryA - salaryB;
-    }
-);
-
-const esPar = (numero) =>{
-    return (numero % 2 ===0) ;
-}
-
-const calcularMediaAritmetica = (lista) => {
-    //let sumaLista = 0;
-
-    // for (let i = 0; i < lista.length; i++) {
-    //     sumaLista = sumaLista + lista[i];
-    // }
-    
-    const sumaLista = lista.reduce(
-        function (valorAcumulado = 0, nuevoElemento){
-            return valorAcumulado + nuevoElemento;
-        }
-    );
-
-    const promedioLista = sumaLista / lista.length;
-    
-    return promedioLista;
-}
-
-const medianaSalarios = (lista) =>{
-    const mitad = parseInt(lista.length/2);
-
-    if (esPar(lista.length)) {
-        const personaMitad1=lista[mitad - 1];
-        const personaMitad2=lista[mitad ];
-        
-        const mediana = calcularMediaAritmetica([personaMitad1,personaMitad2]);
-        return mediana;
-    }else{
-        const personaMitad = lista[mitad];
-        return personaMitad;
-    }
-}
-
-const medianaGeneralCol = medianaSalarios(salariosColSorted)
-
-//mediana del top 10%
-const spliceStart = (salariosColSorted.length * 90)/100;
-const spliceCount = salariosColSorted.length - spliceStart;
-
-const salariosColTop10 = salariosColSorted.splice(spliceStart,spliceCount);
-
-const medianaColTop10 = medianaSalarios(salariosColTop10);
-
-console.log({
-    medianaGeneralCol,
-    medianaColTop10,
-})
-
+  }
